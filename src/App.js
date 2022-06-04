b41541209c39e8c3c2627f490bfe7b583be7d514
prem311297 <premkumar1010kr@gmail.com>
2022-06-04 13:41:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Statewise from "./components/statewise/stateWiseData/statewise";
+
+const App = () => {
+  return (
+      <>
+        <Statewise />
+      </>
+  )
+}
+
+export default App;
